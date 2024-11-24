1b5aff00ef5964599017b9bc8bdd52e32e2be864
User <User@LAPTOP-IEO8ART2>
2024-11-24 13:37:50 +0800

    Add project files.

---
+++ LibraryManagementAPI/Data/LibraryManagementContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Data
+{
+    public class LibraryManagementContext : DbContext
+    {
+        public LibraryManagementContext(DbContextOptions<LibraryManagementContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
